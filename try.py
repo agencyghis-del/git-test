6ba65f53519456e73dbb00273ee03cf8356c0438
Ghis Agency <agencyghis@gmail.com>
2026-09-08 14:23:29 +0100

    'regrouping print statements into a function'

--- try.py
+++ try.py
@@ -1,3 +1,4 @@
-print('Hello, World!')
-print('This is a test script.')
-print('An online commit')
+def print_values():
+    print('Hello, World!')
+    print('This is a test script.')
+    print('An online commit')
